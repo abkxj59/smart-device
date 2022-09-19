f00b50c0d01424c44a3e09ea0cd096962d57c989
Philipp Mendogralo <abkxj59@gmail.com>
2022-09-19 17:08:41 +0500

    Добавляет маску телефона, правки по линтеру

--- source/js/modules/tel-mask.js
+++ source/js/modules/tel-mask.js
@@ -1,38 +1,16 @@
-window.addEventListener("DOMContentLoaded", function () {
-  [].forEach.call(document.querySelectorAll('input[type=tel]'), function (input) {
-    let keyCode;
-    function mask(event) {
-      event.keyCode && (keyCode = event.keyCode);
-      const pos = this.selectionStart;
-      if (pos < 3) event.preventDefault();
-      let matrix = '+7 (___) ___ ____';
-      let i = 0;
-      let def = matrix.replace(/\D/g, '');
-      let val = this.value.replace(/\D/g, '');
-      let newValue = matrix.replace(/[_\d]/g, function (a) {
-        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
-      });
-      i = newValue.indexOf('_');
-      if (i !== -1) {
-        // i < 5 && (i = 3);
-        newValue = newValue.slice(0, i);
-      }
-      let reg = matrix.substr(0, this.value.length).replace(/_+/g,
-          function (a) {
-            return '\\d{1,' + a.length + '}';
-          }).replace(/[+()]/g, '\\$&');
-      reg = new RegExp('^' + reg + '$');
-      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
-        this.value = newValue;
-      };
-      if (event.type === 'blur' && this.value.length < 5) {
-        this.value = '';
-      }
-    }
+import IMask from 'imask';
 
-    input.addEventListener('input', mask, false);
-    input.addEventListener('focus', mask, false);
-    input.addEventListener('blur', mask, false);
-    input.addEventListener('keydown', mask, false);
+const telInputs = document.querySelectorAll('[data-mask="phone"]');
+
+const maskOptions = {
+  mask: '+{7}(000)000-00-00',
+};
+
+const initTelMask = () => {
+  telInputs.forEach((input) => {
+    const addMask = new IMask(input, maskOptions);
+    addMask();
   });
-});
+};
+
+export {initTelMask};
