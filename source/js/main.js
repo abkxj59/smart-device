f86938c348a8f771383ee4290f54d8063df9ee79
Philipp Mendogralo <abkxj59@gmail.com>
2022-10-12 23:46:51 +0500

    for build

--- source/js/main.js
+++
@@ -1,9 +0,0 @@
-import {initAboutToggle} from './modules/about-toggle.js';
-import {initFooterAccordion} from './modules/footer-accordion.js';
-import {initModal} from './modules/modal.js';
-import {initTelMask} from './modules/tel-mask.js';
-
-initModal();
-initAboutToggle();
-initFooterAccordion();
-initTelMask();
